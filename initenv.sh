a8e83b690add6c3152aa24570a0ab7f41442f5df
ggh-png <0380089@gmail.com>
2023-08-03 23:28:19 +0900

    add TTS

--- initenv.sh
+++ initenv.sh
@@ -3,6 +3,8 @@ echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MOD
 
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", MODE:="0666", GROUP:="dialout",  SYMLINK+="yacyac/motor"' >/etc/udev/rules.d/yacyac_motor.rules
 
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6014", ATTRS{serial}=="FT4TCVL4", GROUP:="dialout",  SYMLINK+="yacyac/servo"' >/etc/udev/rules.d/yacyac_servo.rules
+
 service udev reload
 sleep 2
 service udev restart
---
+++ yacyac_io/yacyac_io/my_first_node.py
@@ -0,0 +1,7 @@
+
+def main():
+    print('Hi from yacyac_io.')
+
+
+if __name__ == '__main__':
+    main()
